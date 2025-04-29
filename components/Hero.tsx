2877a22684f7d062b514c3840be817f8538606a1
Reeyt Treat <colyer.mcelmurray@icloud.com>
2025-04-29 00:45:37 -0400

    simplified hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,28 +1,11 @@
 'use client'
 
-import { motion } from 'framer-motion'
-
 export const Hero = () => {
   return (
-    <section className="relative z-10 flex flex-col justify-center items-center text-center min-h-[60vh] px-4 space-y-6">
-      <motion.h1
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 1 }}
-      className="text-4xl md:text-6xl font-bold leading-tight tracking-tight animate-text-gradient"
-    >
-      Welcome to NextBet
-    </motion.h1>
-
-
-      <motion.p
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ delay: 0.3, duration: 1 }}
-        className="text-md md:text-lg text-gray-400 max-w-2xl"
-      >
-        Smarter sports picks, fantasy domination, and elite sportsbook edges — all in one community.
-      </motion.p>
+    <section className="min-h-[60vh] flex flex-col justify-center items-center text-center space-y-4 px-4">
+      <h1 className="text-4xl md:text-6xl font-bold leading-tight tracking-tight animate-text-gradient">
+        Welcome to NextBet
+      </h1>
     </section>
   )
 }
